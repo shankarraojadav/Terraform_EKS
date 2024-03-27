0a7940e10454684123ac7f342e685a4ca58ec0d3
SHANKAR JADAV <shankarjadav360@gmail.com>
2024-03-27 15:19:22 +0530

    changes

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -61,7 +61,7 @@ resource "aws_security_group" "Jenkins-sg" {
 resource "aws_instance" "web" {
   ami                    = "ami-007020fd9c84e18c7"
   instance_type          = "t2.medium"
-  key_name               = "jadhav.pem"
+  key_name               = "jadhav"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
   iam_instance_profile   = aws_iam_instance_profile.example_profile.name
